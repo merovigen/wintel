1b1c1be51bf7a6e22915b1ef0ad729b4c7605e00
Kirill Dubrovin <k.dubrovin@sprinthost.ru>
2017-05-08 01:06:17 +0300

    rewrite daemon mode, add disabling, add separate page to check cid

--- core.py
+++ core.py
@@ -9,19 +9,22 @@ import re
 import logging
 import json
 import datetime
+import glob
+import subprocess
 
 from flask import Flask
 app = Flask(__name__)
 
 # TODO test locking
 
-with open('config.json') as f:
-    config = json.load(f)
+with open('config.json') as config_file:
+    config = json.load(config_file)
 
 db = SqliteDatabase(config['db_file_path'].encode('utf8'))
 logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s',
                     level=logging.INFO,
                     filename=config['log_file_path'].encode('utf8'))
+state_machine_list = []
 
 
 class Message(Model):
@@ -144,13 +147,14 @@ def __system_scan():
         if 'ttyUSB' in dev and int(re.search(r'ttyUSB(.*)', dev).group(1)) % 2 == 0:
             modem_list.append('/dev/'+dev)
     return modem_list
-    # return ['/dev/ttyUSB0']
+    # return ['/dev/ttyUSB0', '/dev/ttyUSB2']
 
 
 def __generate_gammu_config(modem_path):
     # generate temp config file for each modem and returns system absolute path, eg /var/conf/$id.conf
-    config_path = '{}/gammu.conf'.format(config['tmp_config_dir'].encode('utf8'))
-    with open(config_path, 'w') as f:
+    gammu_config_path = '{}/gammu_{}.conf'.format(config['tmp_config_dir'].encode('utf8'),
+                                            re.search('/dev/(.*)', modem_path).group(1))
+    with open(gammu_config_path, 'w') as f:
         f.write("""[gammu]
 port = {}
 connection = at115200
@@ -158,7 +162,7 @@ synchronizetime = yes
 logformat = errorsdate
 gammucoding = utf8
 """.format(modem_path))
-    return config_path
+    return gammu_config_path
 
 
 def __logger(message):
@@ -172,6 +176,16 @@ def __logger(message):
     pass
 
 
+def disable_modem(state_machine):
+    # udevadm returns string like this:
+    # '/devices/pci0000:00/0000:00:1d.0/usb2/2-1/2-1.2/2-1.2:1.0/ttyUSB0/tty/ttyUSB0\n'
+    # we need this part:                         ^^^^^
+    usb_path = subprocess.check_output('udevadm info -q path -n {}'.format(state_machine.GetConfig()['Device']),
+                                       shell=True).rsplit('/', 5)[1]
+    # disable device
+    subprocess.call('echo 1 > /sys/bus/usb/drivers/usb/{}/remove'.format(usb_path), shell=True)
+
+
 def read_sms_by_modem(state_machine):
     status = state_machine.GetSMSStatus()
 
@@ -182,12 +196,12 @@ def read_sms_by_modem(state_machine):
 
     while message_number > 0:
         if start:
-            cursms = state_machine.GetNextSMS(Start=True, Folder=0)
+            message = state_machine.GetNextSMS(Start=True, Folder=0)
             start = False
         else:
-            cursms = state_machine.GetNextSMS(Location=cursms[0]['Location'], Folder=0)
-        message_number -= len(cursms)
-        sms_list.append(cursms)
+            message = state_machine.GetNextSMS(Location=message[0]['Location'], Folder=0)
+        message_number -= len(message)
+        sms_list.append(message)
     return sms_list
 
 
@@ -195,13 +209,7 @@ def read_sms():
     with open('/tmp/wintel.lock', 'w') as lock:
         with flock.Flock(lock, flock.LOCK_EX):
             try:
-                # First, we must do a system scan and find all modems
-                for modem_path in __system_scan():
-                    config_path = __generate_gammu_config(modem_path)
-                    state_machine = init(config_path)
-                    if state_machine is None:
-                        logging.error('Unable to init modem on {}'.format(modem_path))
-                        break
+                for state_machine in state_machine_list:
                     imsi = int(state_machine.GetSIMIMSI())
                     for message in read_sms_by_modem(state_machine):
                         message = message[0]
@@ -224,24 +232,19 @@ def read_sms():
                                 message['DateTime'])
                         # delete message from modem
                         else:
-                            print message
+                            # print message
                             if config['delete_messages']:
                                 state_machine.DeleteSMS(0, message['Location'])
                     # delete temp config file
-                    os.remove(config_path)
+                    # os.remove(config_path)
             except IOError:
                 logging.warning('Lock exists, previous run might be working, exiting.')
                 print 'Lock exists, previous run might be working, exiting.'
 
 
-@cli.command()
+@app.route('/cid')
 def update_cid():
-    for modem_path in __system_scan():
-        config_path = __generate_gammu_config(modem_path)
-        state_machine = init(config_path)
-        if state_machine is None:
-            logging.error('Unable to init modem on {}'.format(modem_path))
-            break
+    for state_machine in state_machine_list:
         imsi = int(state_machine.GetSIMIMSI())
         network_info = state_machine.GetNetworkInfo()
         try:
@@ -258,13 +261,29 @@ def update_cid():
                     network_info['CID'],
                     last_cid,
                     imsi))
+                if config['paranoid_mode']:
+                    disable_modem(state_machine)
+                    state_machine_list.remove(state_machine)
+                    logging.warning('Paranoid mode on, disabled modem with IMSI {}!'.format(imsi))
+    # flask require callable object to be returned
+    return ''
 
 
 @cli.command()
 def web():
     log = logging.getLogger('werkzeug')
     log.setLevel(logging.ERROR)
+    #
+
+    for modem_path in __system_scan():
+        config_path = __generate_gammu_config(modem_path)
+        state_machine_list.append(init(config_path))
+    logging.info('Application started')
     app.run(host=config['web_address'])
+    logging.info('Application stopped')
+    # post cleanup, delete all config
+    for gammu_file in glob.glob('{}/*.conf'.format(config['tmp_config_dir'])):
+        os.remove(gammu_file)
 
 
 @app.after_request
@@ -273,14 +292,14 @@ def treat_as_plain_text(response):
     return response
 
 
-@app.route('/')
+@app.route('/sms')
 def test():
     read_sms()
     messages = ''
     for message in Message.select(Message, Number).join(Number, on=(Message.imsi == Number.imsi).alias('num')).order_by(Message.timestamp.desc()):
         messages += '{} [{}] +7{} | \n'.format(message.sender, datetime.datetime.fromtimestamp(message.timestamp),
-                                            message.num.number,
-                                            message.num.description)
+                                               message.num.number,
+                                               message.num.description)
         messages += '{} \n'.format(message.content.encode('utf-8'))
         messages += '\n'
     return messages
